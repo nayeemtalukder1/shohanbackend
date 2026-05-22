7363ab8140aa98612813eb4b81a3d281deb4aab7
nayeemtalukder1 <nayeemtalukder882@gmail.com>
2026-05-22 15:10:04 +0600

    add details code

--- index.js
+++ index.js
@@ -16,34 +16,62 @@ const client = new MongoClient(uri, {
   }
 });
 
-let database, portfoliosCollection;
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+// let database, portfoliosCollection;
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
 
+
+//     database = client.db("shohanur");
+//     portfoliosCollection = database.collection("portfolio");
+
+//     app.get('/portfolio', async (req, res) => {
+//       const cursor = portfoliosCollection.find({});
+//       const result = await cursor.toArray();
+//       res.json(result);
+//     })
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+let database, portfoliosCollection;
+
+// connect once
+async function connectDB() {
+  if (!database) {
+    await client.connect();
 
     database = client.db("shohanur");
     portfoliosCollection = database.collection("portfolio");
 
-    app.get('/portfolio', async (req, res) => {
-      const cursor = portfoliosCollection.find({});
-      const result = await cursor.toArray();
-      res.json(result);
-    })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log("✅ MongoDB Connected");
   }
 }
-run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/portfolio', async (req, res) => {
+  try {
+    await connectDB();
+
+    const result = await portfoliosCollection.find({}).toArray();
+    res.json(result);
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
