b4287d6b7438fe00798e4085748826bf70a072b7
nayeemtalukder1 <nayeemtalukder882@gmail.com>
2026-05-22 23:18:25 +0600

    add corse

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri =process.env.MONGO_URI;
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // or "http://localhost:3000"
+  })
+);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
