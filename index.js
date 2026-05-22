e68d6d20f218a894787b6b2caf562a27a4d0e787
nayeemtalukder1 <nayeemtalukder882@gmail.com>
2026-05-22 23:21:28 +0600

    import cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require("cors");
 const app = express()
 const port = process.env.PORT;
 const { MongoClient, ServerApiVersion } = require('mongodb');
